9ed205db4d748c512acd16a44a14b3b6b8343c26
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2021-02-13 17:02:14 -0800

    child class assignment

---
+++ Class_pet.py
@@ -0,0 +1,13 @@
+class Pets:
+    #Defining attributes of the class
+     name = '  '
+
+#"child" class PetInfo
+class PetInfo(Pets):
+    pet_age = ' '
+    pet_species = ' '
+
+#"child" class Pet_care
+class Pet_care(Pets)
+    diet = ' '
+    enrichment = ''
