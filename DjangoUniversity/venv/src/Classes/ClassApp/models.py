81f1c000a6633a2eb4e12b5a19f3cc5b207e8b96
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2021-03-16 18:09:13 -0700

    Django University

---
+++ DjangoUniversity/venv/src/Classes/ClassApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class djangoClasses(models.Model):
+    title = models.CharField(max_length=60)
+    course_number = models.IntegerField(null=True, blank=True)
+    instructor_name = models.CharField(max_length=60)
+    duration = models.FloatField(null=True, blank=True)
+
+
