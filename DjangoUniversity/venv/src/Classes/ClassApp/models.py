351ea1353970244c2b83e152dd0cd011eed6f449
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2021-03-18 18:27:13 -0700

    Django University update

--- DjangoUniversity/venv/src/Classes/ClassApp/models.py
+++ DjangoUniversity/venv/src/Classes/ClassApp/models.py
@@ -7,4 +7,7 @@ class djangoClasses(models.Model):
     instructor_name = models.CharField(max_length=60)
     duration = models.FloatField(null=True, blank=True)
 
+    objects = models.Manager()
 
+    def __str__(self):
+        return self.title
